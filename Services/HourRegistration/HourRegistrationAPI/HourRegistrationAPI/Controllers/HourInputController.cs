b134d93fb3b69bacc2540569e8f7a8c87a9a40f2
Lisa <lisa.vankessel@student.fontys.nl>
2020-03-08 23:45:47 +0100

    Clean + add first controller

---
+++ Services/HourRegistration/HourRegistrationAPI/HourRegistrationAPI/Controllers/HourInputController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HourRegistrationAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HourInputController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> PostHours()
+        {
+            return new string[] { "It works"};
+        }
+
+    }
+}
