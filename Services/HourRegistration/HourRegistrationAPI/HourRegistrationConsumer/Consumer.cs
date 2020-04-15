9b53b045f53ddc23764fbc1c22ead09fb6ca2d21
liespies5 <eanvankessel@gmail.com>
2020-04-16 01:20:12 +0200

    Write to database works, make producer slightly more robust

--- Services/HourRegistration/HourRegistrationAPI/HourRegistrationConsumer/Consumer.cs
+++ Services/HourRegistration/HourRegistrationAPI/HourRegistrationConsumer/Consumer.cs
@@ -14,53 +14,48 @@ namespace HourRegistrationConsumer
     {
         static public void Main()
         {
-            HourRegistrationModel model = new HourRegistrationModel();
-            model.CompanyId = Guid.NewGuid().ToString();
-            model.Description = "blablabla";
-            model.EmployeeId = Guid.NewGuid().ToString();
-            model.EndTime = DateTime.Parse("2020-04-05T11:50Z");
-            model.StartTime = DateTime.Parse("2020-04-05T07:22Z");
-            model.ProjectId = Guid.NewGuid().ToString();
+           
+            var factory = new ConnectionFactory() { HostName = "rabbitmq" };
+            factory.AutomaticRecoveryEnabled = true;
 
-            ISession cassandraSession = GetCassandraSession();
+            IConnection connection = GetRabbitMqConnection(factory);
+            Cluster cluster = Cluster.Builder()
+                .AddContactPoint("cassandra")
+                .Build();
 
-            var preparedStatement = cassandraSession.Prepare("INSERT INTO uurtjefactuurtje.hours (id, company_id, project_id, employee_id, start_date, start_time, end_date, end_time, description) VALUES (uuid(), uuid(), uuid(), uuid(), ?, ?, ?, ?, ?)");
 
-            WriteToDatabase(model, cassandraSession, preparedStatement);
+            using (connection)
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "IncomingHourRegistrationMessages",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
 
-            //var factory = new ConnectionFactory() { HostName = "rabbitmq" };
-            //factory.AutomaticRecoveryEnabled = true;
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        HourRegistrationModel retrievedModel = GetModelFromBody(body);
 
-            //IConnection connection = GetRabbitMqConnection(factory); 
+                        ISession cassandraSession = GetCassandraSession(cluster);
+                        var preparedStatement = cassandraSession.Prepare("INSERT INTO uurtjefactuurtje.hours (id, company_id, project_id, employee_id, start_date, start_time, end_date, end_time, description) VALUES (uuid(), ?, ?, ?, ?, ?, ?, ?, ?)");
 
-            //using (connection)
-            //{
-            //    using (var channel = connection.CreateModel())
-            //    {
-            //        channel.QueueDeclare(queue: "IncomingHourRegistrationMessages",
-            //                             durable: false,
-            //                             exclusive: false,
-            //                             autoDelete: false,
-            //                             arguments: null);
+                        WriteToDatabase(retrievedModel, cassandraSession, preparedStatement);
+                        Console.WriteLine(retrievedModel.Description);
+                    };
 
-            //        var consumer = new EventingBasicConsumer(channel);
-            //        consumer.Received += (model, ea) =>
-            //        {
-            //            var body = ea.Body;
-            //            var message = Encoding.UTF8.GetString(body);
-            //            HourRegistrationModel retrievedModel = GetModelFromBody(body);
-            //            WriteToDatabase(retrievedModel);
-            //            Console.WriteLine(retrievedModel.Description);
-            //        };
+                    channel.BasicConsume(queue: "IncomingHourRegistrationMessages",
+                                         autoAck: true,
+                                         consumer: consumer);
 
-            //        channel.BasicConsume(queue: "IncomingHourRegistrationMessages",
-            //                             autoAck: true,
-            //                             consumer: consumer);
-
-            //        Console.WriteLine(" Press [enter] to exit.");
-            //        Console.ReadLine();
-            //    }
-            //}
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
         }
 
         static HourRegistrationModel GetModelFromBody(byte[] body)
@@ -80,19 +75,15 @@ namespace HourRegistrationConsumer
             bool success = true;
             try
             {
-                string startDateBeforeConv = model.StartTime.ToString("yyyy-dd-mm");
-                string endDateBeforeConv = model.StartTime.ToString("yyyy-dd-mm");
-                string startTimeBeforeConv = model.StartTime.ToString("hh:mm:ss");
-                string endTimeBeforeConv = model.EndTime.ToString("hh:mm:ss");
-                LocalDate startDate = LocalDate.Parse(startDateBeforeConv);
-                LocalDate endDate = LocalDate.Parse(endDateBeforeConv);
-                LocalTime startTime = LocalTime.Parse(startTimeBeforeConv);
-                LocalTime endTime = LocalTime.Parse(endTimeBeforeConv);
-                //var statement = preparedStatement.Bind(model.CompanyId, model.ProjectId, model.EmployeeId, model.StartTime, model.EndTime, model.Description);
-                var statement = preparedStatement.Bind(startDate, startTime, endDate, endTime, model.Description);
+                LocalDate startDate = LocalDate.Parse($"{model.StartTime.Year}-{model.StartTime.Month}-{model.StartTime.Day}");
+                LocalDate endDate = LocalDate.Parse($"{model.EndTime.Year}-{model.EndTime.Month}-{model.EndTime.Day}");
+                LocalTime startTime = LocalTime.Parse($"{model.StartTime.Hour}:{model.StartTime.Minute}:{model.StartTime.Second}");
+                LocalTime endTime = LocalTime.Parse($"{model.EndTime.Hour}:{model.EndTime.Minute}:{model.EndTime.Second}");
+                Guid emp = Guid.Parse(model.EmployeeId);
+                Guid com = Guid.Parse(model.CompanyId);
+                Guid proj = Guid.Parse(model.ProjectId);
+                var statement = preparedStatement.Bind(com, proj, emp, startDate, startTime, endDate, endTime, model.Description);
                 session.Execute(statement);
-                //session.Execute("INSERT INTO uurtjefactuurtje.hours (id, company_id, project_id, employee_id, start_time, end_time, description) " +
-                //                        $"VALUES {model.CompanyId}, {model.ProjectId}, {model.EmployeeId}, {model.StartTime}, {model.EndTime}, {model.Description}");
             }
             catch (Exception e)
             {
@@ -122,7 +113,7 @@ namespace HourRegistrationConsumer
             return connection;
         }
 
-        static ISession GetCassandraSession()
+        static ISession GetCassandraSession(Cluster cluster)
         {
             bool success = false;
             ISession session = null; 
@@ -131,10 +122,6 @@ namespace HourRegistrationConsumer
             {
                 try
                 {
-                    Cluster cluster = Cluster.Builder()
-                        .AddContactPoint("cassandra")
-                        .Build();
-
                     session = cluster.Connect();
                     success = true;
                 }
