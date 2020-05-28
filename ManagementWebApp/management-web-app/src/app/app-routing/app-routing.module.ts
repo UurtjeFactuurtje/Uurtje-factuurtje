f3e6a021cd1b75c38df3b52517897f9dd4c67393
liespies5 <eanvankessel@gmail.com>
2020-05-28 19:41:29 +0200

    Add routing

--- ManagementWebApp/management-web-app/src/app/app-routing/app-routing.module.ts
+++ ManagementWebApp/management-web-app/src/app/app-routing/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProjectsComponent } from '../projects/projects.component';
+import { LoginComponent } from '../login/login.component';
 
 const routes: Routes = [
-  //{ path: 'heroes', component: HeroesComponent }
+  { path: '', component: ProjectsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: '**', component: ProjectsComponent }
 ];
 
 @NgModule({
